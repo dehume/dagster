1b2c6a49602ff9e2e2f16cf32a17da8b72bff19b
Sandy Ryza <sandy@elementl.com>
2021-08-27 13:53:56 -0700

    InProcessSolidResult.output_value

--- python_modules/dagster/dagster/core/execution/execution_results.py
+++ python_modules/dagster/dagster/core/execution/execution_results.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List, Optional, cast
 
 from dagster import DagsterEvent, check
 from dagster.core.definitions import GraphDefinition, Node, NodeHandle, SolidDefinition
+from dagster.core.definitions.utils import DEFAULT_OUTPUT
 from dagster.core.errors import DagsterInvariantViolationError
 from dagster.core.execution.plan.outputs import StepOutputHandle
 from dagster.core.execution.plan.step import StepKind
@@ -103,8 +104,34 @@ class InProcessSolidResult(NodeExecutionResult):
                             ] = value
                     else:
                         results[step_output_handle.output_name] = value
+        else:
+            raise DagsterInvariantViolationError(
+                "Trying to fetch output values, but output_capturing_enabled was False for the run"
+            )
 
         return results
+
+    def output_value(self, output_name=DEFAULT_OUTPUT):
+        """Get a computed output value.
+
+        Note that calling this method will reconstruct the pipeline context (including, e.g.,
+        resources) to retrieve materialized output values.
+
+        Args:
+            output_name(str): The output name for which to retrieve the value. (default: 'result')
+
+        Returns:
+            Union[None, Any, Dict[str, Any]]: ``None`` if execution did not succeed, the output value
+                in the normal case, and a dict of mapping keys to values in the mapped case.
+        """
+        check.str_param(output_name, "output_name")
+
+        if not self._output_capture:
+            raise DagsterInvariantViolationError(
+                "Trying to fetch output values, but output_capturing_enabled was False for the run"
+            )
+
+        return self._output_capture[StepOutputHandle(str(self.handle), output_name)]
 
     @property
     def event_list(self) -> List[DagsterEvent]:
